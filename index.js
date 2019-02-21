086bbb23d2c76d92d58512fe2ef95a0d72f36e6b
Seito Taka <amanoese@gmail.com>
2019-02-21 22:24:53 +0900

    fix output

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const execa = require('execa')
 const suggestExec = async (input) => {
   let {stdout,cmd} = result  = await execa.shell(input);
   let title = stdout.split('\n').slice(0,20).join('\n')
-  return [ { title , value: input } ]
+  return [ { title:input , value: input },{ title , value: input } ].filter(v=>v.title)
 }
 
 autocompletePrompt('gsh', suggestExec)
-  //.on('data', (e) => console.log('miss exec', e.value))
-  .on('submit', (v) => console.log(v))
+//.on('data', (e) => console.log('miss exec', e.value))
+.on('submit', (v) => {
+  execa.shell(v).stdout.pipe(process.stdout)
+})
