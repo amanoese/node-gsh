8883299de44369b7b59fd576fb51a8d47d8d1901
Seito Taka <amanoese@gmail.com>
2019-06-12 14:09:10 +0900

    test convert ava

--- test/shellExecDocker.js
+++ test/shellExecDocker.js
@@ -1,7 +1,9 @@
+const test = require('ava');
 let shellExecDocker = require('../src/shellExecDocker');
 
-(async () =>{
+test('docker run',async t =>{
   //let outstr = await shellDockerExec.exec('echo Hello,World!')
   let outstr = await shellExecDocker.exec('seq 20 | factor')
   console.log(JSON.stringify(outstr,null,'  '));
-})();
+  t.pass();
+});
